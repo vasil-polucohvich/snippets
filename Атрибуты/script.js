d212d63a81bb67527f24febd1ca223fc818be75e
vasil-polucohvich <69108694+vasil-polucohvich@users.noreply.github.com>
2021-11-23 09:39:13 +0200

    Update script.js

--- Атрибуты/script.js
+++ Атрибуты/script.js
@@ -5,8 +5,8 @@
 var a = document.querySelector('a')
 var oldHref = a.getAttribute('href')
 
-a.setAttribute('href', 'https://google.com')
-a.setAttribute('title', 'Go to google')
-a.textContent = 'Google'
+a.setAttribute('href', 'https://ya.ru')
+a.setAttribute('title', 'Go to yandex')
+a.textContent = 'Yandex'
 
 console.log(a.attributes)
