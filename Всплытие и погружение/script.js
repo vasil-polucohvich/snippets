2a46d4d1b229967555ae33b6547d2837fe1ca74d
vasil-polucohvich <69108694+vasil-polucohvich@users.noreply.github.com>
2021-11-23 10:19:18 +0200

    Update script.js

---
+++ Всплытие и погружение/script.js
@@ -0,0 +1,14 @@
+var divs = document.querySelectorAll('div')
+
+for(var i = 0; i < divs.length; i++){
+	divs[i].addEventListener('click',function(){
+		console.log(this.getAttribute('id'))
+
+	})
+}
+// for(var i = 0; i < divs.length; i++){
+// 	divs[i].addEventListener('click',function(){
+// 		console.log(this.getAttribute('id'))
+
+// 	},true)
+// }
